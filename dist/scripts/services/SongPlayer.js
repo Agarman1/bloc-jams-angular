5d990f4b31f24a5e65092dba87b3a070d6544bb5
Dan <Agarman1@users.noreply.github.com>
2017-03-13 22:59:36 -0500

    Assignment 7 Services Part 2: completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,24 +2,55 @@
     function SongPlayer() {
          var SongPlayer = {};
 
+         /**
+         * @desc Sets current song
+         * @type {Object}
+         */
+
          var currentSong = null;
+
+         /**
+         * @desc Buzz object audio file
+         * @type {Object}
+         */
+
          var currentBuzzObject = null;
+
+         /**
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
+
+         var setSong = function(song) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.stop();
+                 currentSong.playing = null;
+             }
+
+             currentBuzzObject = new buzz.sound(song.audioUrl, {
+                 formats: ['mp3'],
+                 preload: true
+             });
+
+             currentSong = song;
+          };
+
+          /**
+          * @function playSong
+          * @desc Plays song and sets song.playing to true
+          * @param {Object} song
+          */
+
+          var playSong = function(song) {
+             currentBuzzObject.play();
+             song.playing = true;
+          }
 
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
-                 if (currentBuzzObject) {
-                     currentBuzzObject.stop();
-                     currentSong.playing = null;
-                 }
-                 currentBuzzObject = new buzz.sound(song.audioUrl, {
-                     formats: ['mp3'],
-                     preload: true
-                 });
-
-                 currentSong = song;
-
-                 currentBuzzObject.play();
-                 song.playing = true;
+                 setSong(song);
+                 playSong(song);
 
              } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
