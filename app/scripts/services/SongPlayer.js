435ca5537263ca5afccf6bd38ddd3ed1e2cabcb0
Dan <Agarman1@users.noreply.github.com>
2017-03-24 14:14:34 -0500

    Assignment 8 Services Part 3: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -24,8 +24,10 @@
 
          var setSong = function(song) {
              if (currentBuzzObject) {
-                 currentBuzzObject.stop();
-                 SongPlayer.currentSong.playing = null;
+               /** currentBuzzObject.stop();
+               * SongPlayer.currentSong.playing = null;
+               */
+               stopSong(song);
              }
 
              currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -45,6 +47,17 @@
           var playSong = function(song) {
              currentBuzzObject.play();
              song.playing = true;
+          }
+
+          /**
+          * @function stopSong
+          * @desc Stops song and sets song.playing to null
+          * @param {Object} song
+          */
+
+          var stopSong = function(song) {
+             currentBuzzObject.stop();
+             SongPlayer.currentSong.playing = null;
           }
 
           /**
@@ -94,8 +107,34 @@
               currentSongIndex--;
 
               if (currentSongIndex < 0) {
-                  currentBuzzObject.stop();
-                  SongPlayer.currentSong.playing = null;
+                  /** currentBuzzObject.stop();
+                  * SongPlayer.currentSong.playing = null;
+                  */
+                  stopSong(song);
+
+              } else {
+                  var song = currentAlbum.songs[currentSongIndex];
+                  setSong(song);
+                  playSong(song);
+              }
+
+          };
+
+          SongPlayer.next = function() {
+
+            /**
+            * @desc Holds current song index
+            * @type {Object}
+            */
+
+              var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+              currentSongIndex++;
+
+              if (currentSongIndex > currentAlbum.songs.length) {
+                /** currentBuzzObject.stop();
+                * SongPlayer.currentSong.playing = null;
+                */
+                stopSong(song);
               } else {
                   var song = currentAlbum.songs[currentSongIndex];
                   setSong(song);
