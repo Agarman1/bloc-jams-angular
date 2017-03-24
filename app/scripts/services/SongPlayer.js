7a3a45b3a12e7ef6d33cd05a0c73c750e3e45b4a
Dan <Agarman1@users.noreply.github.com>
2017-03-24 11:14:01 -0500

    Checkpoint 8 Services Part 3: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -92,17 +92,17 @@
 
               var currentSongIndex = getSongIndex(SongPlayer.currentSong);
               currentSongIndex--;
+
+              if (currentSongIndex < 0) {
+                  currentBuzzObject.stop();
+                  SongPlayer.currentSong.playing = null;
+              } else {
+                  var song = currentAlbum.songs[currentSongIndex];
+                  setSong(song);
+                  playSong(song);
+              }
+
           };
-
-          if (currentSongIndex < 0) {
-              currentBuzzObject.stop();
-              SongPlayer.currentSong.playing = null;
-          } else {
-              var song = currentAlbum.songs[currentSongIndex];
-              setSong(song);
-              playSong(song);
-          }
-
 
           return SongPlayer;
     }
