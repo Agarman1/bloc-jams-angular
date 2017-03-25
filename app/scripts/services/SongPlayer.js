7d4696e8dae18d833ed67b107fd68a62bfc4b1b1
Dan <Agarman1@users.noreply.github.com>
2017-03-25 16:49:30 -0500

    Checkpoint 10 Directives Part 2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
 
         /**
@@ -33,6 +33,12 @@
              currentBuzzObject = new buzz.sound(song.audioUrl, {
                  formats: ['mp3'],
                  preload: true
+             });
+
+             currentBuzzObject.bind('timeupdate', function() {
+                 $rootScope.$apply(function() {
+                     SongPlayer.currentTime = currentBuzzObject.getTime();
+                 });
              });
 
              SongPlayer.currentSong = song;
@@ -76,6 +82,12 @@
           */
 
           SongPlayer.currentSong = null;
+
+          /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          */
+          SongPlayer.currentTime = null;
 
           SongPlayer.play = function(song) {
              song = song || SongPlayer.currentSong;
@@ -143,10 +155,23 @@
 
           };
 
+          /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          */
+          SongPlayer.setCurrentTime = function(time) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.setTime(time);
+              }
+          };
+
           return SongPlayer;
     }
 
+
+
     angular
           .module('blocJams')
-          .factory('SongPlayer', ['Fixtures', SongPlayer]);
+          .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -39,7 +39,9 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
@@ -50,6 +52,14 @@
                 */
 
                 var seekBar = $(element);
+
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
 
                 /**
                 * @function percentString
@@ -99,6 +109,7 @@
 
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
 
                 scope.trackThumb = function() {
@@ -110,15 +121,22 @@
                         */
 
                         var percent = calculatePercent(seekBar, event);
-
+                        scope.$apply(function() {
                             scope.value = percent * scope.max;
-
+                            notifyOnChange(scope.value);
+                        });
                     });
 
                     $document.bind('mouseup.thumb', function() {
                         $document.unbind('mousemove.thumb');
                         $document.unbind('mouseup.thumb');
                     });
+                };
+
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
 
             }
